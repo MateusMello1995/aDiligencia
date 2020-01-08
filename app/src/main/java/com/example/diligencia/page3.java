017996c319414b8bb8b0a71777bf2df02e1c2b50
Visitante7 <mellowmateus@outlook.com>
2020-01-08 11:41:34 -0300

    adicionado toolbar a pege3

--- app/src/main/java/com/example/diligencia/page3.java
+++ app/src/main/java/com/example/diligencia/page3.java
@@ -14,7 +14,8 @@ public class page3 extends AppCompatActivity {
         setContentView(R.layout.activity_page3);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
-        getSupportActionBar().setTitle("A Diligência");
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
 
 
 
