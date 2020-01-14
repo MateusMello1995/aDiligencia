b3f896f505ed982a7f1fd43200fb9d6eb691e197
Visitante7 <mellowmateus@outlook.com>
2020-01-14 11:02:05 -0300

    adicionado drawer com sucesso!!!!

--- app/src/main/java/com/example/diligencia/page3.java
+++ app/src/main/java/com/example/diligencia/page3.java
@@ -1,22 +1,39 @@
 package com.example.diligencia;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class page3 extends AppCompatActivity {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page3);
+
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, myToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
+
+    }
+
+    public void proximaTela(View view){
+
+        Intent intent = new Intent(this, page4_drawer.class);
+        startActivity(intent);
 
 
     }
